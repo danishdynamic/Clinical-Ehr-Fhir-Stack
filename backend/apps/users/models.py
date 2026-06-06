7f51f6cacfb40df93c5cae80726ebe1ce7d37ced
danishdynamic <shakeali.ali@gmail.com>
2026-06-06 17:37:41 +0530

    feat(auth): add custom user model and PostgreSQL integration

---
+++ backend/apps/users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class UserRole(models.TextChoices):
+    ADMIN = "ADMIN", "Admin"
+    DOCTOR = "DOCTOR", "Doctor"
+    NURSE = "NURSE", "Nurse"
+    PATIENT = "PATIENT", "Patient"
+    AUDITOR = "AUDITOR", "Auditor"
+
+class User(AbstractUser):
+    role = models.CharField(max_length=20, choices=UserRole.choices, default=UserRole.PATIENT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
